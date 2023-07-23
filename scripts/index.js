bcd4b4c383c49b4b431facbca2871621b9371b74
Эльдар <ethakohov@yandex.ru>
2023-07-23 16:18:40 +0300

    Пр-6

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,22 @@ const profileEditButton = document.querySelector(".profile__edit");
 const popupOpenedProfile = document.querySelector(".popup_type_profile");
 const buttonCloseProfile = popupOpenedProfile.querySelector(".popup__close");
 
-const popup = document.querySelector(".popup");
+const popup = Array.from(document.querySelectorAll(".popup"));
 
 const popupProfileForm = document.querySelector(".popup__form_type_profile");
 const nameInput = document.querySelector(".popup__input_type_name");
+const spanName = document.querySelector(".name-error");
 const jobInput = document.querySelector(".popup__input_type_job");
+const spanJob = document.querySelector(".job-error");
 
 const profileAuthor = document.querySelector(".profile__author");
 const profileDescription = document.querySelector(".profile__description");
 
 const cards = document.querySelector(".cards");
 const cardsList = document.querySelector(".cards__list");
-const cardTemplate = document.querySelector("#card").content.querySelector(".card");
+const cardTemplate = document
+  .querySelector("#card")
+  .content.querySelector(".card");
 const card = document.querySelector(".card");
 
 const popupAddCard = document.querySelector(".popup_type_add-card");
@@ -27,7 +31,14 @@ const popupCaption = popupTypeImage.querySelector(".popup__caption");
 const popupImageCloseButton = popupTypeImage.querySelector(".popup__close");
 
 const inputPlace = popupCardForm.querySelector(".popup__input_type_place");
+const spanPlace = document.querySelector(".place-error");
 const inputSrc = popupCardForm.querySelector(".popup__input_type_src");
+const spanSrc = document.querySelector(".src-error");
+
+function hideInputErrors(span, input) {
+  span.classList.remove("popup__error_visible");
+  input.classList.remove("popup__input_type_error");
+}
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -89,6 +100,8 @@ function profileFormSubmit(evt) {
 }
 
 profileEditButton.addEventListener("click", () => {
+  hideInputErrors(spanName, nameInput);
+  hideInputErrors(spanJob, jobInput);
   openPopup(popupOpenedProfile);
   nameInput.value = profileAuthor.textContent;
   jobInput.value = profileDescription.textContent;
@@ -103,9 +116,10 @@ popupProfileForm.addEventListener("submit", profileFormSubmit);
 popupCardForm.addEventListener("submit", addNewCard);
 
 addButton.addEventListener("click", () => {
+  hideInputErrors(spanPlace, inputPlace);
+  hideInputErrors(spanSrc, inputSrc);
   openPopup(popupAddCard);
-  inputPlace.value = "";
-  inputSrc.value = "";
+  popupCardForm.reset();
 });
 
 buttonCloseCard.addEventListener("click", () => {
@@ -115,3 +129,21 @@ buttonCloseCard.addEventListener("click", () => {
 popupImageCloseButton.addEventListener("click", () => {
   closePopup(popupTypeImage);
 });
+
+popup.forEach((item) => {
+  item.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("popup_opened")) {
+      closePopup(item);
+    }
+  });
+});
+
+popup.forEach((item) => {
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      closePopup(item);
+    }
+  });
+});
+
+enableValidation(validationConfig);
